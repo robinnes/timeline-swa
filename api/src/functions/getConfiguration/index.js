d42ce43d35968a3dcec966ed29ed8ace8390951e
robinnes <wrob@comcast.net>
2026-07-08 13:11:13 -0500

    Correct api getConfiguration function syntax

--- api/src/functions/getConfiguration/index.js
+++ api/src/functions/getConfiguration/index.js
@@ -1,11 +1,14 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      environment: process.env.APP_ENVIRONMENT
-    }
-  };
-};
+const { app } = require('@azure/functions');
+
+app.http('getConfiguration', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    return {
+      status: 200,
+      jsonBody: {
+        environment: process.env.APP_ENVIRONMENT || 'production'
+      }
+    };
+  }
+});
