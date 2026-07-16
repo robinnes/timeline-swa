c10eaca3a1fa161122a089dae9b8fb92007d16e0
robinnes <wrob@comcast.net>
2026-07-15 20:59:19 -0500

    Expose permissions to the client.

--- api/src/functions/getConfiguration/index.js
+++ api/src/functions/getConfiguration/index.js
@@ -1,13 +1,25 @@
 const { app } = require('@azure/functions');
 
+const {
+  canPublish,
+  canUseThumbnails,
+  getUserRoles
+} = require("../authorization");
+
 app.http('getConfiguration', {
   methods: ['GET'],
   authLevel: 'anonymous',
   handler: async (request, context) => {
+
+    const roles = await getUserRoles(req);
+
     return {
       status: 200,
       jsonBody: {
-        environment: process.env.APP_ENVIRONMENT || 'production'
+        environment: process.env.APP_ENVIRONMENT,
+        roles,
+        canPublish: await canPublish(req),
+        canUseThumbnails: await canUseThumbnails(req)
       }
     };
   }
